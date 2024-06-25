aa501f5dfe52d9fa463f6e20ef3d066657feb7d4
“Vishakha2814” <vishakhadhamgunde555@gmail.com>
2024-06-25 17:04:54 +0530

    Java Main.java file is added

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+        String names [] = new String[5];
+        for (int x=0; x < args.length; x++)
+            names[x] = args[x];
+        System.out.println(names[4]);
+    }
+}
